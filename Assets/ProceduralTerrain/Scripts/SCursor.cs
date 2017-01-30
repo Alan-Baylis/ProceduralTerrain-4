07d66a2a58d6a5a5bb0f6b565dd9d9d38b06a229
Soufian <soufiazzouz@laposte.net>
2017-01-30 21:02:36 +0100

    Hide cursor, teaser tests

---
+++ Assets/ProceduralTerrain/Scripts/SCursor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SCursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Set Cursor to not be visible
+        Cursor.visible = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
